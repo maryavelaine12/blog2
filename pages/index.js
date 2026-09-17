b92b54eb0a69a8c8c16dea175467556cccdd17bd
Mary Avelaine Buenaventura <madbuenaventura.student@ua.edu.ph>
2026-09-17 15:12:49 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+<Head>
+  <title>Mary Avelaine D. Buenaventura</title>
+  <meta name="description" content="My personal Next.js blog" />
+  <link rel="icon" type="image/png" href="/favicon.png" />
+</Head>
+
+      <main className={styles.main}>
+        <Image
+          src="/images/profile.jpg"
+          alt="Profile Picture"
+          width={150}
+          height={150}
+          style={{ borderRadius: '50%', objectFit: 'cover' }}
+        />
+
+        <h1 className={styles.title}>
+          Mary Avelaine D. Buenaventura
+        </h1>
+
+        <section style={{ maxWidth: '700px', marginTop: '30px' }}>
+          <h2>Why I Chose My IT Specialization</h2>
+
+          <p>
+            I chose Data Analytics because I am interested in how data can be
+            used to understand problems and make better decisions. Today,
+            businesses and organizations collect a lot of data, and they need
+            people who can turn that data into useful information.
+          </p>
+
+          <p>
+            What made me interested in this field is seeing how data is used in
+            real-life situations, such as understanding customer behavior,
+            improving business performance, and predicting future trends. I
+            want to improve my skills in analyzing data and use them to solve
+            real-world problems in the future.
+          </p>
+
+          <p>
+            (This is a sample website - you’ll be building a site like this on our Next.js tutorial.)
+          </p>
+        </section>
+      </main>
+    </div>
+  )
+}
